0ccaba34445cf535cf343442c77fa7599e062a69
Daniel Santos <122944322+daniel-lgs@users.noreply.github.com>
2023-03-16 23:35:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import pandas
 
 nato_phonetic_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 npq_dic = {row["letter"]: row["code"] for (index, row) in nato_phonetic_alphabet.iterrows()}
-text = input("Enter a word: ").upper()
-corresponding_words = [npq_dic[letter] for letter in text]
-print(corresponding_words)
+while True:
+    try:
+        text = input("Enter a word: ").upper()
+        corresponding_words = [npq_dic[letter] for letter in text]
+    except KeyError:
+        print("Just letters and a single word, please.")
+    else:
+        print(corresponding_words)
